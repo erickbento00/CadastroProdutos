33fbcd672ab4f2079f0dae0aacee7ced32a00f24
Erick Bento <clev.familia@gmail.com>
2021-05-13 17:31:51 -0300

    Adicionando fpdf

--- imprimir.php
+++ imprimir.php
@@ -1,10 +1,42 @@
 <?php
 require("libs/fpdf/fpdf.php");
-    $pdf = new FPDF('P','mm','A4');
-    $pdf->AddPage();
-    $pdf->SetFont('Arial','B',16);
-    $pdf->Cell(40, 10, 'Opa, imprime ai!');
-    $pdf->Cell(40, 60,'Hello World !', 1, 1);
-    $pdf->Cell(60, 80, "Olá Mundo!", 0, 1, 'C');
-    $pdf->Output();
+
+class PDF extends FPDF
+{
+// Cabeçalho da Página
+function Header()
+{
+    // Logo
+    $this->Image('imagens/php.png', 10, 6, 20);
+    // Arial bold 15
+    $this->SetFont('Arial', 'B', 15);
+    // Movendo para direita
+    $this->Cell(80);
+    // Titulo
+    $this->Cell(30, 10, 'PDF', 1, 0, 'C');
+    // Quebra de linha
+    $this->Ln(20);
+}
+
+// Rodapé da Página
+function Footer()
+{
+// Posicição a 1,5 cm da parte inferior
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I', 8);
+    // Número da Página
+    $this->Cell(0, 10, 'Page ' . $this->PageNo() . '/{nb}', 0, 0, 'C');
+}
+}
+
+// Instancias de classes Herdadas
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Times', '', 12);
+for($i=1;$i<=10;$i++){
+    $pdf->Cell(0, 5, 'Mostrando linha ' . $i, 0, 1);
+};
+$pdf->Output();
 ?>
